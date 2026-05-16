1661b781cdb87e37061991c70a0d1109bd2b1b33
Ghulam Fatah Barefi <barefig@gmail.com>
2026-05-16 23:05:13 +0700

    Create Medication Room Entity with soft delete tracking

---
+++ app/src/main/java/com/barefi0012/asesmen2/data/Medication.kt
@@ -0,0 +1,14 @@
+package com.barefi0012.asesmen2.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "medications")
+data class Medication(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String,
+    val dosage: String,
+    val time: String,
+    val isTaken: Boolean = false,
+    val isDeleted: Boolean = false
+)
